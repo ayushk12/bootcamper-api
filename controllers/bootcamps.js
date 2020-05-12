a59fdaf1cf51203e0f34f452756a65e2d3f1d632
ayush kumar <kayush242@gmail.com>
2020-05-12 15:43:19 +0530

    sortig the query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,44 @@ const geocoder = require("../utils/geocoder");
 // @routes GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ["select"];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operator ($gt,$gte,etc)
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select) {
+    const fileds = req.query.select.split(",").join(" ");
+    query = query.select(fileds);
+  }
+
+  // Sort the query
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Executing query
+  const bootcamps = await query;
 
   res
     .status(200)
